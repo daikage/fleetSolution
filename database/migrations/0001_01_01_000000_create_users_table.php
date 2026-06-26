5585eea5a5a3a652621ad92a58945b8a8381f8bf
daikage <30565500+daikage@users.noreply.github.com>
2026-06-26 10:27:10 +0100

    Reg

--- database/migrations/0001_01_01_000000_create_users_table.php
+++ database/migrations/0001_01_01_000000_create_users_table.php
@@ -15,7 +15,7 @@ return new class extends Migration
             $table->id();
             $table->string('name');
             $table->string('email')->unique();
-            $table->string('role')->default('driver'); // 'admin' or 'driver'
+            $table->string('role')->default('admin'); // 'admin' or 'driver'
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
             $table->rememberToken();
